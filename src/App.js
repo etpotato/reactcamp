460c2a49b91c9a7963ce9be42aa13bac7830266f
etpotato <mareev.pv@gmail.com>
2021-08-14 23:51:52 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import './styles.css';
 
-
-fetch("https://randomuser.me/api/?results=3")
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-});
+const URL = 'https://randomuser.me/api/?results=';
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
+  const [cardsCount, setCardsCount] = useState(3);
+  const changeCount = (increase) => {
+    if (increase) {
+      setCardsCount(cardsCount + 1);
+      return
+    }
+    if (cardsCount === 0) return;
+    setCardsCount(cardsCount - 1);
+  };
+  const getContacts = () => {
+    return fetch(URL + cardsCount)
+    .then(response => response.json())
+    .then(data => {
+        setContacts(data.results);
+    });
+  };
+  useEffect(() => {
+    getContacts();
+  }, [cardsCount]);
   return (
     <>
+      {/* <button onClick={getContacts}>Update cards</button> */}
+      <div>
+        <button onClick={() => changeCount(true)}>Add</button>
+          <span>{cardsCount}</span>
+        <button onClick={() => changeCount(false)}>Remove</button>
+      </div>
       {contacts.map(contact => (
         <ContactCard
-          avatar = 'https://via.placeholder.com/150'
-          name = {contact.name}
+          key = {contact.login.uuid}
+          avatar = {contact.picture.large}
+          name = {contact.name.first + ' ' + contact.name.last}
           email = {contact.email}
-          age = {contact.age}
+          age = {contact.dob.age}
         />
       ))}
     </>
@@ -26,7 +47,6 @@ const App = () => {
 
 const ContactCard = (props) => {
   const [showAge, setShowAge] = useState(true);
-
   return (
     <div className="contact-card">
       <img src={props.avatar} alt="user" />
